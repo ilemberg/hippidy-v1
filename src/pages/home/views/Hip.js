887b5569aa13c49be3b4a77c61e364de9c5ff5d3
Ilya Lemberg <ilemberg@levelforward.com>
2024-10-09 20:24:44 -0400

    v1 of the visual of a hip. is this enough to keep going with the functionality?

--- src/pages/home/views/Hip.js
+++ src/pages/home/views/Hip.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaExpandAlt } from 'react-icons/fa';
+import { FaExpandAlt , FaRocket, FaReply, FaSearch, FaStar } from 'react-icons/fa';
 import { Bar } from 'react-chartjs-2';
 import {
   Chart as ChartJS,
@@ -59,7 +59,7 @@ const Hip = () => {
 
       data.datasets[0].data.forEach((value, index) => {
         const xPosition = x.getPixelForValue(index);
-        const yPosition = y.getPixelForValue(value) - 5; // 5 pixels above the bar
+        const yPosition = y.getPixelForValue(value) - 5;
         ctx.fillText(`${value}%`, xPosition, yPosition);
       });
       ctx.restore();
@@ -68,21 +68,44 @@ const Hip = () => {
 
   return (
     <div className="hip-card">
-      <div className="hip-top-row">
-        <div className="hip-profile-pic"></div>
-        <FaExpandAlt className="hip-expand-icon" />
+    <div className="hip-top-row">
+      <div className="hip-profile-pic"></div>
+      <FaExpandAlt className="hip-expand-icon" />
+    </div>
+    <div className="hip-question-row">
+      <h2 className="hip-question">Who will you vote for president in the 2024 election?</h2>
+    </div>
+    <div className="hip-secondary-row">
+      <p className="hip-secondary-question political">What political party do you belong to?</p>
+      <p className="hip-secondary-question religious">What religion do you identify with?</p>
+    </div>
+    <div className="hip-button-groups">
+      <div className="hip-button-set">
+        <button className="hip-button political">Dem</button>
+        <button className="hip-button hip-button-selected political">Rep</button>
+        <button className="hip-button political">Ind</button>
+        <button className="hip-button political">Other</button>
       </div>
-      <div className="hip-question-row">
-        <h2 className="hip-question">Who will you vote for president in the 2024 election?</h2>
+      <div className="hip-button-set">
+        <button className="hip-button religious">Christ</button>
+        <button className="hip-button religious">Jewish</button>
+        <button className="hip-button hip-button-selected religious">Atheist</button>
+        <button className="hip-button religious">Other</button>
       </div>
-      <div className="hip-secondary-row">
-        <p className="hip-secondary-question">What political party do you belong to?</p>
-        <p className="hip-secondary-question">What religion do you identify with?</p>
-      </div>
+    </div>
+    <div className="hip-chart-container">
+      <div className="hip-chart-label">Rep & Atheist</div>
       <div className="hip-bar-chart">
         <Bar data={chartData} options={chartOptions} plugins={plugins} />
       </div>
     </div>
+    <div className="hip-footer">
+        <button className="hip-footer-button"><FaRocket /> Boost</button>
+        <button className="hip-footer-button"><FaReply /> Answer</button>
+        <button className="hip-footer-button"><FaSearch /> Investigate</button>
+        <button className="hip-footer-button"><FaStar /> Favorite</button>
+      </div>
+  </div>
   );
 };
 
